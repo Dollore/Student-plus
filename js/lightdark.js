5a38367609a39e2fe259a5b535f63da68d226c8a
sartezzo <149673585+sartezzo@users.noreply.github.com>
2023-12-17 21:36:47 +0100

    Update lightdark.js

--- js/lightdark.js
+++ js/lightdark.js
@@ -1,13 +1,26 @@
 const colorButton = document.querySelector('.color-button');
 let isLightMode = true;
 
+const savedMode = localStorage.getItem('colorMode');
+if (savedMode) {
+    isLightMode = savedMode === 'light';
+    updateColorMode();
+}
+
 colorButton.addEventListener('click', function () {
+    isLightMode = !isLightMode;
+    updateColorMode();
+
+    // Zapisywanie preferencji użytkownika w local storage
+    localStorage.setItem('colorMode', isLightMode ? 'light' : 'dark');
+});
+
+function updateColorMode() {
     if (isLightMode) {
-        document.body.classList.remove('light-mode');
-        document.body.classList.add('dark-mode');
-    } else {
         document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
+    } else {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
     }
-    isLightMode = !isLightMode;
-});
+}
